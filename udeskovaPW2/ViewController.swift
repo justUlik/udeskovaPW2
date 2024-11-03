22bdb21c7a7eb6bbbfbe35ef7eaf8b1217006c7c
Uliana Eskova <uliana.eskova@gmail.com>
2024-11-03 23:39:10 +0300

    Initial Commit

---
+++ udeskovaPW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  udeskovaPW2
+//
+//  Created by Ulyana Eskova on 03.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
